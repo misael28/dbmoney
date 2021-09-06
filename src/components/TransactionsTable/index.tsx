aa8366a05b04a895ae775105b5542ada8823d219
Souza <38145379+misael28@users.noreply.github.com>
2021-09-05 21:48:55 -0300

    feat: create table transaction

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from "../TransactionsTable/styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de applicação web</td>
+            <td>R$ 12000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de applicação web</td>
+            <td>R$ 12000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de applicação web</td>
+            <td>R$ 12000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
