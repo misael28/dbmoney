1b020d6fe0e7a2a192c3ce0bfb8562a8fb99e68c
Souza <38145379+misael28@users.noreply.github.com>
2021-09-09 23:24:26 -0300

    feat: add button inside modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import Modal from "react-modal";
-import { Container } from "../NewTransactionModal/styles";
+import {
+  Container,
+  TransactionTypeContainer,
+} from "../NewTransactionModal/styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -16,14 +22,34 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type="button">
-        <img src={closeImg} onClick={onRequestClose} alt="fechar modal" />
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="fechar modal" />
       </button>
       <Container>
         <h1>Cadastrar transação</h1>
+
         <input placeholder="Título" />
+
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
+
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
